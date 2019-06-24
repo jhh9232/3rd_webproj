12ad73a05545cae6fafdb060259261ca28b250d5
JeoungHyun-Hwang <ghkd01212@gmail.com>
2019-06-24 14:59:02 +0900

    be_init

---
+++ be/main.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var app = express();
+var router = express.Router();
+var fs = require('fs');
+
+app.set('views', './');
+app.use(express.static(__dirname, + '/public'));
+
+app.listen(3303, function() {
+    console.log('Server start');
+});
+
+app.get('/',function (req, res) {
+    fs.readFile('test.html',function(error,data) {
+        if(error) {
+            console.log(error);
+        }else {
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.end(data);
+        }
+    });
+});
